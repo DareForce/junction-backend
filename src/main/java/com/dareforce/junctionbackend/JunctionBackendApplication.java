b9b7a0ddfda52c88df09e56a90e179a44726d322
julie0005 <julie0005@ajou.ac.kr>
2023-08-18 18:45:35 +0900

    chore : 프로젝트 초기화

---
+++ src/main/java/com/dareforce/junctionbackend/JunctionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dareforce.junctionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunctionBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JunctionBackendApplication.class, args);
+    }
+
+}
